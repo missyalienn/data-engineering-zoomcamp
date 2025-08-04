458056cf683c5f68c6d6b98e8bbc41cf5403c3c6
Chip <allan.missy@gmail.com>
2025-08-04 14:06:00 -0400

    feat: convert ingestion notebook to script and upload data from script

--- 00-workspace/upload-data.py
+++ 00-workspace/upload-data.py
@@ -7,31 +7,61 @@ from time import time
 import pandas as pd 
 from sqlalchemy import create_engine
 
-url = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz"
-csv_name = url.split("/")[-1]
-df = pd.read_csv(csv_name, nrows=100)
-pd.io.sql.get_schema(df, name='yellow_taxi_data')
+def main(params): 
+    user = params.user
+    password = params.password
+    host = params.host
+    port = params.port 
+    db = params.db
+    table_name = params.table_name 
+    url = params.url  
 
-# Convert pickup and dropoff columns from text to datetime objects
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
 
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/nyc_taxi')
+    os.system(f"wget {url} -O {csv_name}")
 
-df_iter = pd.read_csv(csv_name, iterator= True, chunksize=100000)
-df = next(df_iter)
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
 
-#df.head(0).to_sql(name= 'yellow_taxi_data', con= engine, if_exists= 'replace')
-#df.to_sql(name= 'yellow_taxi_data', con= engine, if_exists= 'append')
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
 
-for i, df in enumerate(df_iter): 
+    df = next(df_iter)
+
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    for i, df in enumerate(df_iter, start=1): 
     
-    start_t = time()
+        start_t = time()
 
-    df.to_sql(name='yellow_taxi_data', con=engine, if_exists= 'append')
+        df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+        df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-    end_t = time()
-    print(f'Ingested chunk {i +1}, took {end_t - start_t:.3f}seconds')
+        df.to_sql(name=table_name, con=engine, if_exists= 'append')
 
+        end_t = time()
 
+        print(f'Ingested chunk {i +1}, took {end_t - start_t:.3f}seconds')
+
+if __name__ == '__main__':  
+
+    parser=argparse.ArgumentParser(description="Ingest CSV data to postgres")
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+
+    args = parser.parse_args()
+    main(args)
+
+    ##
+  
