9f967356edc9c67c255cfd37ce5d9dda6cf388c3
Chip <allan.missy@gmail.com>
2025-08-11 15:43:48 -0400

    update terraform config

--- 00-workspace/terraform/main.tf
+++ 00-workspace/terraform/main.tf
@@ -11,3 +11,28 @@ provider "google" {
   project     = "nyc-taxi-data-eng"
   region      = "us-central1"
 }
+
+
+resource "google_storage_bucket" "nyc-taxi-bucket" { #local name of the bucket 
+  name          = "nyc-taxi-data-eng-terra-bucket" #provide unique name for the bucket - can use projectid
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3 #age in days
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
